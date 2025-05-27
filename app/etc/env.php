abe739b3f75f7cc96a6a8ada8db6b47e6a4fafed
Priyam raj <rajpriyam27@gmail.com>
2025-05-27 15:55:06 +0530

    Added full app/ folder with code, design, and etc changes

---
+++ app/etc/env.php
@@ -0,0 +1,66 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_xrwvnh1'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'UTymQdzE7xIvTPOjjJp9rdi4tKX3SoYp'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '29f_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '29f_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'config' => [
+        'async' => 0
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'base6466GljbUiSQVQq+92SxgWQPvigFk9eInPt+TCiBmX9EI='
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2',
+                'username' => 'magento',
+                'password' => 'magento123',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ]
+];
